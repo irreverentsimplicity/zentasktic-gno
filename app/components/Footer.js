e53b8ce78a3390f157370e5345cd2af875f4793a
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-06-14 01:27:45 -0400

    initial commit

---
+++ app/components/Footer.js
@@ -0,0 +1,9 @@
+'use client'
+import { Link } from '@chakra-ui/next-js'
+
+export default function Footer() {
+
+    return (        
+          <div>made with &#x2764;&#xFE0F; by <Link href="https://github.com/irreverentsimplicity">@irreverentsimplicity</Link></div>
+    );
+}
