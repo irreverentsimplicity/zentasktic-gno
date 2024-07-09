5a756bbc2e0e6c528ca44a4c22736ada6e6a476e
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-07-09 04:00:39 -0400

    refactor for dev/prod environments

--- app/util/config.ts
+++ app/util/config.ts
@@ -1,9 +1,9 @@
 export default {
   GNO_WS_URL: "http://localhost:26657/websocket",
-  GNO_JSONRPC_URL: "http://localhost:26657", // local rpc node
-  //GNO_JSONRPC_URL: "http://localhost:36657",  // gno dev
+  GNO_JSONRPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
   GNO_ZENTASKTIC_CORE_REALM: "gno.land/p/demo/zentasktic",
   GNO_ZENTASKTIC_PROJECT_REALM: "gno.land/r/demo/zentasktic_project",
   GNO_ZENTASKTIC_USER_REALM: "gno.land/r/demo/zentasktic_user",
-  FAUCET_URL: "http://127.0.0.1:5050"
+  //GNO_JSONRPC_URL: "http://localhost:36657",  // gno dev
+  FAUCET_URL: process.env.NEXT_PUBLIC_FAUCET_URL,
 };
